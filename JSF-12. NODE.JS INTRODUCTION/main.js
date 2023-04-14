738de7b117a0c70db43dd4ca9a1ace4771d5da62
Nick Sotula <so2niko@gmail.com>
2023-04-14 17:09:17 +0000

    nodejs lesson

---
+++ JSF-12. NODE.JS INTRODUCTION/main.js
@@ -0,0 +1,11 @@
+// getPromise("test promise", 2000).then(function(data) {
+//     console.log(data);        
+// });
+
+// function delayMessage(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// delayMessage("test promise", 2000).then((data) => console.log(data));
+
+console.log('I am a node');
